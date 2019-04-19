22ad50e4a4a8c5521faa55141480c3723dfa9b90
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2019-04-18 20:17:48 -0700

    delete todos and add todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,25 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 }); 
+
+//Click on X to delete todos
+//"event.stopPropagation()"Prevents the event from bubbling up the DOM tree,
+// preventing any parent handlers from being notified of 
+//the event.
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        //clear the input field
+        $(this).val("");
+        //create a new li and add it to ul
+        $("ul").append("<li>" + todoText + "</li>");
+    }
+});
